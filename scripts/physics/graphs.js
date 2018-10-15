09666e9e5fed24cab289f6c1f5dd2e1df2132a7b
SploxFox <sjtl2004@gmail.com>
2018-10-15 14:10:46 -0500

    Update graphs.js

--- scripts/physics/graphs.js
+++ scripts/physics/graphs.js
@@ -2,4 +2,13 @@ class DataSet {
   constructor(dataPoints){
     this.dataPoints = dataPoints;
   }
+  get canvas(width,height){
+    if (width != undefined){
+      this.width = width;
+    }
+    if (height != undefined){
+      this.height = height;
+    }
+    const canvas = document.createElementById("canvas");
+  }
 }
