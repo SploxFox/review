16349806b4c2771da9aa885b5bf8ed7c3f38aa91
SploxFox <sjtl2004@gmail.com>
2018-10-15 14:57:02 -0500

    Update graphs.js

--- scripts/physics/graphs.js
+++ scripts/physics/graphs.js
@@ -2,13 +2,33 @@ class DataSet {
   constructor(dataPoints){
     this.dataPoints = dataPoints;
   }
-  get canvas(width,height){
-    if (width != undefined){
+  graphPosition(width,height,connectDots){
+    const canvas = document.createElementById("canvas");
+    /*if (width != undefined){
       this.width = width;
-    }
+    } //maybe this will be used?
     if (height != undefined){
       this.height = height;
+    }*/
+    canvas.width = width;
+    canvas.height = height;
+    //Creating lines on the graph
+    for (var i = 0; i < this.dataPoints.length; i++){
+      
     }
-    const canvas = document.createElementById("canvas");
+  }
+  //Returns the bounds of the y data set in the form of (min,max)
+  get yBounds(){
+    var max;
+    var min;
+    for (var i = 0; i < this.dataPoints.length; i++){
+      if (max == undefined || max < this.dataPoints.length[i].y){
+        max = this.dataPoints.length[i].y;
+      }
+      if (min == undefined || min > this.dataPoints.length[i].y){
+        min = this.dataPoints.length[i].y;
+      }
+    }
+    return new Vector(min,max);
   }
 }
