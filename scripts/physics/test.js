131f5cfe429412e0611b9af748c04af171e32aaa
SploxFox <sjtl2004@gmail.com>
2018-10-19 12:23:09 -0500

    Create test.js

---
+++ scripts/physics/test.js
@@ -0,0 +1,9 @@
+const data = new DataSet([
+  new Vector(0,0),
+  new Vector(10,10),
+  new Vector(20,20),
+  new Vector(30,30),
+  new Vector(40,40)
+]);
+const graph = data.graph(500,500,false);
+document.body.appendChild(graph.canvas);
